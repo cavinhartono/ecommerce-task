a82d177c5e8b98783fbc7b8d30ccb60508d9b3d6
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-01-13 20:17:45 +0700

    master

--- halaman/cart.php
+++ halaman/cart.php
@@ -9,24 +9,26 @@
 
 <?php
 
-require_once('../algoritma/Config.php');
-require('../algoritma/auth/Login.php');
-
 session_start();
+
+require_once('../algoritma/Config.php');
+
 
 $auth = $_SESSION['auth'];
 
 if (isset($_POST['submit'])) {
-  $statement = $db->prepare("INSERT INTO (`user_id`, `product_id`, `qty`) FROM `carts` VALUES (:user_id, :product_id, :qty)");
+  $statement = $db->prepare("INSERT INTO carts(`user_id`, `product_id`, `qty`) VALUES (:user_id, :product_id, :qty)");
   $statement->bindParam(':product_id', $_POST['product_id']);
   $statement->bindParam(':user_id', $auth);
   $statement->bindParam(':qty', $_POST['qty']);
   $statement->execute();
+  header("Location: ./cart.php");
 }
 
-$carts = $db->prepare("SELECT `name`, `price`, `src` FROM `products` INNER JOIN `carts` ON `products`.`id` = `carts`.`product_id`")->fetchAll(PDO::FETCH_ASSOC);
+$statement = $db->prepare("SELECT `name`, `price`, `src` FROM `products` INNER JOIN `carts` ON `products`.`id` = `carts`.`product_id` WHERE `carts`.`user_id` = $auth");
+$statement->execute();
 
-var_dump($carts);
+$carts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -35,7 +37,7 @@ var_dump($carts);
     <ul class="flex flex-col gap-6">
       <?php foreach ($carts as $cart) : ?>
         <li class="w-full flex gap-6">
-          <?php if ($cart->status == 'pending') : ?>
+          <!-- <?php if ($cart->status == 'pending') : ?>
             <span class="text-red-600 bg-red-300 border-red-600 rounded-md">
               Pesanan akan datang ke tujuan
             </span>
@@ -43,11 +45,11 @@ var_dump($carts);
             <span class="text-green-600 bg-green-300 border-green-600 rounded-md">
               Pesanan Anda sudah sampai tujuan
             </span>
-          <?php endif ?>
-          <img src="./gambar/{$cart->src}" class="w-[100px] h-[100px]">
+          <?php endif ?> -->
+          <img src="./gambar/<?= $cart['src'] ?>" class="w-[100px] h-[100px]">
           <div class="flex flex-col gap-4">
-            <h1 class="text-black text-lg font-serif">{$cart->name}</h1>
-            <h1 class="text-gray-800 text-md">{$cart->price}</h1>
+            <h1 class="text-black text-lg font-serif"><?= $cart['name'] ?></h1>
+            <h1 class="text-gray-800 text-md">Rp. <?= number_format($cart['price'], 0, ".", ".") ?></h1>
           </div>
         </li>
       <?php endforeach; ?>
